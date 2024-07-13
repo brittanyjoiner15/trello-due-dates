389f3f3ef14c791c8d1e4216e76369960d516bd1
Brittany Joiner <brittanyjoiner15@gmail.com>
2024-07-13 16:58:34 -0500

    add console with card stuff

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,6 +2,11 @@ console.log("Im building a powerup!")
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
-      return []
+      return t.card('all').then(function (card) {
+        console.log(card);
+        return [{
+          text: card.name,
+        }];
+      });
     }
   });
