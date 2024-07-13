e0e0d9ed8a75e728f142fa0d643128feb3e998cf
Brittany Joiner <brittanyjoiner15@gmail.com>
2024-07-13 17:01:52 -0500

    get checklists

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,7 @@ console.log("Im building a powerup!")
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
       return t.card('all').then(function (card) {
-        console.log(card);
+        console.log(card.checklists);
         return [{
           text: card.name,
         }];
