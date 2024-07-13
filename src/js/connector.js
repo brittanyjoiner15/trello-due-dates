8ecd3bc9e0d6da546573cb220b072f9b68eb6888
Brittany Joiner <brittanyjoiner15@gmail.com>
2024-07-13 17:32:34 -0500

    fetch checklist data

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,11 +2,25 @@ console.log("Im building a powerup!")
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
-      return t.card('all').then(function (card) {
+      return t.card('all')
+      .then(function (card) {
         console.log(card.checklists);
-        return [{
-          text: card.name,
-        }];
+        if (card.checklists.length > 0) {
+            // if it has a checklist, lets look up checklist information
+            return fetch(`https://api.trello.com/1/cards/${card.id}/checklists=all?key=%%APP_KEY%%&token=%%APP_TOKEN%%`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function(checklistData) {
+                console.log(checklistData);
+                return [{
+                    text: `Checklist: ${checklistData[0].name}`,
+                }]
+            })
+        }
+
+        return [];
       });
     }
-  });
+});
+
