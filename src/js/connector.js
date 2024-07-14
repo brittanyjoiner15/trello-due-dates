545edc6831a3482d8d340672f5ade154be417717
Brittany Joiner <brittanyjoiner15@gmail.com>
2024-07-14 13:02:28 -0500

    format and test returning incomplete item

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,24 +2,28 @@ console.log("Im building a powerup!")
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
-      return t.card('all')
-      .then(function (card) {
-        console.log(card.checklists);
-        if (card.checklists.length > 0) {
-            // if it has a checklist, lets look up checklist information
-            return fetch(`https://api.trello.com/1/cards/${card.id}/?checklists=all&key=%%APP_KEY%%&token=%%APP_TOKEN%%`)
-            .then(function(response) {
-                console.log("response", response)
-                return response.json();
-            })
-            .then(function(checklistData) {
-                console.log("checklistdata", checklistData.checklists[0].checkItems);
-                return [];
-            })
-        }
+        return t.card('all')
+            .then(function (card) {
+                console.log(card.checklists);
+                if (card.checklists.length > 0) {
+                    // if it has a checklist, lets look up checklist information
+                    return fetch(`https://api.trello.com/1/cards/${card.id}/?checklists=all&key=%%APP_KEY%%&token=%%APP_TOKEN%%`)
+                        .then(function (response) {
+                            console.log("response", response)
+                            return response.json();
+                        })
+                        .then(function (checklistData) {
+                            const checklistItems = checklistData.checklists[0].checkItems;
+                            const incompleteChecklistItems = checklistItems.filter(item => item.state === "incomplete");
+                            console.log("incomplete items", checklistItems.filter(item => item.state === "incomplete"))
+                            return [{
+                                text: incompleteChecklistItems[0].name,
+                            }]
+                        })
+                }
 
-        return [];
-      });
+                return [];
+            });
     }
 });
 
